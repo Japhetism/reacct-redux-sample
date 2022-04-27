8c74c8034d418f517b0c6efca642fdcf30d56c9b
Babatunde Ojo <babatundeojo30@gmail.com>
2022-04-27 07:37:32 +0100

    react-redux-selector

--- src/pages/currency/index.tsx
+++ src/pages/currency/index.tsx
@@ -1,42 +1,17 @@
 import React, { useEffect } from "react";
-import { Dispatch } from "redux";
-import { connect } from "react-redux";
-import { bindActionCreators } from "redux";
-
-import fetchCurrenciesService from "../../services/currencies";
-import {
-  getCurrenciesLoading,
-  getCurrencies,
-  getCurrenciesError
-} from "../../redux/reducers/currencies";
-import { Currency, CurrencyResponseState } from '../../redux/reducers/currencies';
-
+import fetchCurrencies from "../../services/currencies";
+import { useDispatch } from "react-redux";
+import { useTypedSelector } from "../../hooks/useTypeSelector";
 import CurrencyScreen from "./screen";
 
-interface Props {
-  currencies: Currency[],
-  fetchCurrencies(): void,
-}
-
-const CurrencyContainer = (props:Props) => {
-  const { currencies, fetchCurrencies } = props;
+const CurrencyContainer = () => {
+  const dispatch = useDispatch();
+  const { currencies } = useTypedSelector((state:any) => state.currencies);
+  
   useEffect(() => {
-    fetchCurrencies();
-  }, [fetchCurrencies]);
+    dispatch(fetchCurrencies() as any)
+  }, [dispatch])
   return <CurrencyScreen currencies={currencies}/>
 };
 
-const mapStateToProps = (state:CurrencyResponseState) => ({
-  error: getCurrenciesError(state),
-  currencies: getCurrencies(state),
-  loading: getCurrenciesLoading(state),
-})
-
-const mapDispatchToProps = (dispatch:Dispatch) => bindActionCreators({
-  fetchCurrencies: fetchCurrenciesService
-}, dispatch)
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(CurrencyContainer);
+export default CurrencyContainer;
